a0ea6f6b47b92a120b0a57fc36138cd1526da2dd
lulian666 <644139490@qq.com>
2022-12-23 21:05:25 +0800

    filter and sort

--- contollers/products.js
+++ contollers/products.js
@@ -1,10 +1,33 @@
-
+const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: 'getAll testing route' })
+    // const products = await Product.find({ name: { $regex: 'ab', $options: 'i' } })
+    const products = await Product.find({}).sort('-name price')
+    res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'getAll' })
+    // const products = await Product.find(req.query)
+    const { name, featured, company, sort } = req.query
+    queryObject = {}
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
